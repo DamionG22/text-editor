a447ffdc8080cfb6117269c4d736efdae77a4eea
blue butter <damiongallarza@gmail.com>
2023-08-30 19:33:03 -0700

    added asset cache is client side folder src-sw file.

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,20 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+
+  ({ request }) => request.destination === 'style' || request.destination === 'script',
+
+  new CacheFirst({
+    cacheName: 'assets-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0,200],
+      }),
+      new ExpirationPlugin({
+        // caches assets for 7 days 
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
